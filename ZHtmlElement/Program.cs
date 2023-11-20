0f168a9b25d26832dc48ec24dd9408ca76b7bb71
kdanzp <kdanzp@yandex.ru>
2023-11-20 10:55:04 +0500

    Добавьте файлы проекта.

---
+++ ZHtmlElement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Resources;
+using System.Text;
+using ZennoLab.CommandCenter;
+using ZennoLab.Emulation;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using ZennoLab.InterfacesLibrary.ProjectModel.Enums;
+
+namespace ZHtmlElement
+{
+    /// <summary>
+    /// Класс для запуска выполнения скрипта
+    /// </summary>
+    public class Program : IZennoExternalCode
+    {
+        /// <summary>
+        /// Метод для запуска выполнения скрипта
+        /// </summary>
+        /// <param name="instance">Объект инстанса выделеный для данного скрипта</param>
+        /// <param name="project">Объект проекта выделеный для данного скрипта</param>
+        /// <returns>Код выполнения скрипта</returns>		
+        public int Execute(Instance instance, IZennoPosterProjectModel project)
+        {
+            int executionResult = 0;
+
+            return executionResult;
+        }
+    }
+}
